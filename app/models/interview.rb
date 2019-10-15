1b29da39bdcabe528b1107f2767bc595f59d3516
Whitny Edwards <wne4e2@gmail.com>
2019-10-14 22:32:38 -0400

    create method to display date in month day year format

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,5 +1,9 @@
 class Interview < ApplicationRecord
     # belongs_to :user
     has_many :reviews
+
+    def pretty_created_date
+        created_at.strftime("%b %d, %Y")
+    end
     
 end
--- app/serializers/interview_serializer.rb
+++ app/serializers/interview_serializer.rb
@@ -1,4 +1,4 @@
 class InterviewSerializer < ActiveModel::Serializer
-  attributes :id, :company_name, :salary, :day, :position, :location
+  attributes :id, :company_name, :salary, :pretty_created_date, :position, :location
   has_many :reviews
 end
